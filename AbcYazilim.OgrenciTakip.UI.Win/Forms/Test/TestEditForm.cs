4e29c78c7ee42a75cecc05c375256ac7b071b9a8
Rami Koco <kocorami@gmail.com>
2025-04-15 17:43:50 +0300

    Güncelle 15.04.2025 17:43

---
+++ AbcYazilim.OgrenciTakip.UI.Win/Forms/Test/TestEditForm.cs
@@ -0,0 +1,12 @@
+﻿using AbcYazilim.OgrenciTakip.UI.Win.Forms.BaseForms;
+
+namespace AbcYazilim.OgrenciTakip.UI.Win.Forms.Test
+{
+    public partial class TestEditForm : BaseEditForm
+    {
+        public TestEditForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AbcYazilim.OgrenciTakip.UI.Win/Forms/Test/TestListForm.cs
@@ -0,0 +1,12 @@
+﻿using AbcYazilim.OgrenciTakip.UI.Win.Forms.BaseForms;
+
+namespace AbcYazilim.OgrenciTakip.UI.Win.Forms.Test
+{
+    public partial class TestListForm : BaseListForm
+    {
+        public TestListForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
